6055342220f73a3d996b9ba1bc00e06eb5310b46
alfoly <amir.alfoly@gmail.com>
2016-10-31 12:07:52 +0200

    Created Club class

---
+++ src/main/java/csd/starter/Club.java
@@ -0,0 +1,13 @@
+package csd.starter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by amir on 10/31/2016.
+ */
+public class Club {
+    public List<Court> courts = new ArrayList<Court>();
+
+    public static Club ActiveClub = new Club();
+}
