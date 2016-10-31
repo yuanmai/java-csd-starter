97681f7007f70696e6bb660c6821ac7ffb87af4a
yollaek <ollaek@gmail.com>
2016-10-31 17:26:32 +0200

    add reservationResponse class

---
+++ src/main/java/csd/starter/ReservationResponse.java
@@ -0,0 +1,10 @@
+package csd.starter;
+
+/**
+ * Created by yollaek on 10/31/2016.
+ */
+public class ReservationResponse  extends ResponseBase{
+    public ReservationResponse(String message, boolean success) {
+        super(message, success);
+    }
+}
