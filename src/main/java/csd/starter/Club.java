61697595a8b539044f7dc3911a2991a7edfba53d
alfoly <amir.alfoly@gmail.com>
2016-10-31 12:50:17 +0200

    Created Main Application

--- src/main/java/csd/starter/Club.java
+++ src/main/java/csd/starter/Club.java
@@ -19,4 +19,15 @@ public class Club {
         System.out.println("Reservation from "+resev.from.toString() +" to "+resev.to.toString() +" done to user "+resev.user.name);
         return true;
     }
+
+    public void PrintReservationLog()
+    {
+        System.out.println("========================================");
+        System.out.println("Reservation Log:");
+        for(int i=0; i<sheet.size(); i++)
+        {
+            Reservation resev = (Reservation)sheet.get(i);
+            System.out.println("Reservation from "+resev.from.toString() +" to "+resev.to.toString() +" done to user "+resev.user.name);
+        }
+    }
 }
--- src/main/java/csd/starter/Main.java
+++ src/main/java/csd/starter/Main.java
@@ -6,12 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Player tennisPlayer = new Player();
-//        Manager manager = new Manager();
-//        PremiumPlayer prePlayer = new PremiumPlayer();
-//        prePlayer.name = "Ziko";
-//        Court court = new Court();
-//        Reservation resev = new Reservation(new Date(1000), new Date(2000), tennisPlayer, court);
-//        court.Reserve(resev);
+        Player tennisPlayer = new Player("Ahmed");
+        Court court = new Court("CourtA", 10, 10);
+        Club.ActiveClub.courts.add(court);
+
+        Reservation resev = new Reservation(new Date(116, 10, 10, 11, 0), new Date(116, 10, 10, 13, 0), tennisPlayer, court);
+        Club.ActiveClub.Reserve(resev);
+        Club.ActiveClub.PrintReservationLog();
+
+        System.out.print("Nearest court to Player Ahmed is: ");
+        Court nearest = tennisPlayer.GetNearestCourt(1, 1);
+        System.out.println(nearest.Name);
     }
 }
