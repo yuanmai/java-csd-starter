0318bd21fc4d7978140b100eab803f851bb0b5c6
matef1 <mohamed.atef@valeo.com>
2016-10-31 16:48:35 +0200

    added partner confirmation feature

--- src/main/java/csd/starter/Main.java
+++ src/main/java/csd/starter/Main.java
@@ -6,29 +6,30 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main
-{
+public class Main {
     public static databaseEntry databaseObj;
     public static databaseHandler dbHandler = new databaseHandler();
 
     public static void addEntryInDB(List<databaseEntry> entries) throws IOException {
-        try
-        {
+        try {
             dbHandler.addEntry(entries);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String args[]) throws IOException
-    {
+    public static void main(String args[]) throws IOException {
 
+        List<databaseEntry> entries = createDatabaseObj();
+
+        addEntryInDB(entries);
+
+    }
+
+    private static List<databaseEntry> createDatabaseObj() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<databaseEntry> entries = new ArrayList<databaseEntry>();
-        do
-        {
+        do {
             databaseObj = new databaseEntry();
             System.out.println("Enter your username : ");
             String userName = br.readLine();
@@ -43,12 +44,18 @@ public class Main
 
             databaseObj.setRID();
             entries.add(databaseObj);
+            System.out.println("Do you want to send the confirmation to your partner, type yes to insert the number/email, no otherewise");
+            String confrimationSelection = br.readLine();
+            if(confrimationSelection.equals("yes"))
+            {
+                System.out.println("Please enter your partner's email/number :");
+                String input = br.readLine();
+                System.out.println("Confirmation sent successfully to " + input);
+            }
             System.out.println("If you want to add another entry, type yes, otherwise type done.");
-        }while(!br.readLine().equals("done"));
+        } while (!br.readLine().equals("done"));
 
 
-
-        addEntryInDB(entries);
-
+        return entries;
     }
 }
