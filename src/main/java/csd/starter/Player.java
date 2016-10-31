d0e9b05ec2982d541b758e42085ea0af62148563
akamel <ahmed.kamel@valeo.com>
2016-10-31 12:18:23 +0200

    Add 1st User Story

--- src/main/java/csd/starter/Player.java
+++ src/main/java/csd/starter/Player.java
@@ -7,6 +7,16 @@ import java.util.Date;
  */
 public class Player extends User {
 
+    public Player(String name) {
+        this.name = name;
+        this.role = "Player";
+    }
+
+    public Player(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
     void requestReservation(Date from, Date to) {
 
     }
--- src/test/java/csd/starter/MainTest.java
+++ src/test/java/csd/starter/MainTest.java
@@ -1,22 +1,30 @@
 package csd.starter;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
-import java.util.Date;
+import java.util.*;
 
 public class MainTest {
 
     @Test
-    public void test() {
-
-        Player tennisPlayer = new Player();
-        Manager manager = new Manager();
-        PremiumPlayer prePlayer = new PremiumPlayer();
+    public void size_of_empty_queue_should_be_zero() {
+        Player tennisPlayer = new Player("Ahmed");
+        Manager manager = new Manager("Zaazaa");
+        PremiumPlayer prePlayer = new PremiumPlayer("ElFoly");
         Court court = new Court();
-        Reservation resev = new Reservation(new Date(1000), new Date(2000), tennisPlayer, court);
-
-
+        Reservation resev = new Reservation(new Date(116, 10, 10, 11, 0), new Date(116, 10, 10, 13, 0), tennisPlayer, court);
         Assert.assertEquals(true, court.Reserve(resev));
     }
+
+//    @Test
+//    public void size_should_be_increased_by_one_after_pushing() {
+//        queue.add("First Element"); //Act SUT (system/subsystem Under Test)
+//
+//        Assert.assertEquals(1, queue.size()); //Assert
+//    }
+//
+//    @Test(expected = NoSuchElementException.class)
+//    public void popping_an_empty_queue_shall_throw_exception() {
+//        queue.remove();
+//    }
 }
