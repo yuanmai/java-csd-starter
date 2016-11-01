de8cecae4d3c112bf75c17ddbdf6d011c2ecdc06
azaazaa <ahmedzaazaa@gmail.com>
2016-11-01 12:51:14 +0200

    close Scanner

--- src/main/java/csd/starter/UIInteractive.java
+++ src/main/java/csd/starter/UIInteractive.java
@@ -12,61 +12,57 @@ import java.util.Scanner;
 public class UIInteractive {
 
 
-    public void Reserve(String input)
-    {
-        /*
-        Player tennisPlayer = new Player("Ahmed");
+    public void Reserve(String input){
 
-        Reservation resev = new Reservation(new Date(116, 10, 10, 11, 0), new Date(116, 10, 10, 13, 0), tennisPlayer, court);
-        Club.ActiveClub.Reserve(resev);
-        Club.ActiveClub.PrintReservationLog();
+        Scanner scanner =null;
 
-        System.out.print("Nearest court to Player Ahmed is: ");
-        Court nearest = tennisPlayer.GetNearestCourt(1, 1);
-        System.out.println(nearest.Name);
-        */
+        try{
+            if(input != null)
+                scanner = new Scanner(input);
+            else
+                scanner = new Scanner(System.in);
 
-        Scanner scanner;
-        if(input != null)
-            scanner = new Scanner(input);
-        else
-            scanner = new Scanner(System.in);
+            System.out.println("Enter the name of the player: ");
 
-        System.out.println("Enter the name of the player: ");
+            String playerName = scanner.nextLine();
+            Player player = new Player(playerName);
 
-        String playerName = scanner.nextLine();
-        Player player = new Player(playerName);
+            System.out.println("Enter the date and time for the reservation dd-MM-yyyy HH:mm");
+            String dateString = scanner.nextLine();
+            DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+            Date date = null;
+            try {
+                date = formatter.parse(dateString);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return;
+            }
 
-        System.out.println("Enter the date and time for the reservation dd-MM-yyyy HH:mm");
-        String dateString = scanner.nextLine();
-        DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm");
-        Date date = null;
-        try {
-            date = formatter.parse(dateString);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return;
+            Date date2 = new Date(date.getYear(), date.getMonth(), date.getDay(), date.getHours(), date.getMinutes());
+
+            System.out.println("Enter your location x and y");
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            Court nearestCourt = player.GetNearestCourt(x, y);
+            if(nearestCourt != null)
+            {
+                System.out.println("Nearest court is: "+nearestCourt.Name);
+            }
+            else
+            {
+                System.out.println("Can't find a near court");
+                return;
+            }
+
+            Reservation resev = new Reservation(date, date2, player, nearestCourt);
+            Club.ActiveClub.Reserve(resev);
+            Club.ActiveClub.PrintReservationLog();
+
+        }finally {
+            if(scanner != null)
+                scanner.close();
         }
 
-        Date date2 = new Date(date.getYear(), date.getMonth(), date.getDay(), date.getHours(), date.getMinutes());
-
-        System.out.println("Enter your location x and y");
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        Court nearestCourt = player.GetNearestCourt(x, y);
-        if(nearestCourt != null)
-        {
-            System.out.println("Nearest court is: "+nearestCourt.Name);
-        }
-        else
-        {
-            System.out.println("Can't find a near court");
-            return;
-        }
-
-        Reservation resev = new Reservation(date, date2, player, nearestCourt);
-        Club.ActiveClub.Reserve(resev);
-        Club.ActiveClub.PrintReservationLog();
     }
 
     public void SetupData()
